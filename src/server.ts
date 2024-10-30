094800d55afa0bd828541e139a282b46b4658ab9
Brayden O'Neal <braydenoneal@protonmail.com>
2024-10-30 15:47:48 -0500

    Add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import database from './database.js'
 
 const app = express()
 const port = 3000
 
-app.get('/', (_req, res) => {
-    res.status(200).json({ message: 'Hello World' })
+app.get('/', async (_req, res) => {
+    const db = await database.getConnection()
+    const [rows, fields] = await db.execute('SELECT * FROM User');
+    db.release()
+    res.status(200).json(rows)
 })
 
 app.listen(port, () => {
