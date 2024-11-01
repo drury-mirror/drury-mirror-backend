aa82023a0094ef163ae8c39c7e1df0b9326ed498
Brayden O'Neal <braydenoneal@protonmail.com>
2024-11-01 11:04:35 -0500

    Use environment variable for port

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import express from 'express'
 import connection from './database.js'
+import 'dotenv/config'
 
 const app = express()
-const port = 3000
+app.use(express.json())
 
 app.get('/', async (_req, res) => {
-    const [results, _fields] = await connection.query('SELECT * FROM User');
+    const [results, _fields] = await connection.query('SELECT * FROM User')
     res.status(200).json(results)
 })
 
-app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`)
+app.post('/user', async (req, res) => {
+    const body = req.body
+    res.status(200).json(body)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App listening on http://localhost:${process.env.PORT}`)
 })
